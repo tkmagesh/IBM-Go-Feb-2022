0b34d16c47e535444a63c8202eedd3525d613aba
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-21 12:46:54 +0530

    Hello world in go

---
+++ 01-intro/hello-world.go
@@ -0,0 +1,18 @@
+/* package declaration (the package to which this program belongs to) */
+/* The starting point of the application must belong to 'main' package */
+package main
+
+import "fmt"
+
+/* import dependency packages */
+
+/* package level variables & types */
+
+/* package init function */
+
+/* main function */
+func main() {
+	fmt.Println("Hello World!")
+}
+
+/* other functions */
