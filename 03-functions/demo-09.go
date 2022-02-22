13fac207440707ec59a4ffaa9882204b12d1121b
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-22 10:46:48 +0530

    closures

--- 03-functions/demo-09.go
+++ 03-functions/demo-09.go
@@ -2,18 +2,20 @@ package main
 
 import "fmt"
 
-var counter int
-
 func main() {
+	increment := getIncrement()
 	fmt.Println(increment()) //=> 1
 	fmt.Println(increment()) //=> 2
 	fmt.Println(increment()) //=> 3
-	counter = 1000
-	fmt.Println(increment()) //=> 1001
-	fmt.Println(increment()) //=> 1002
+	fmt.Println(increment()) //=> 4
+	fmt.Println(increment()) //=> 5
 }
 
-func increment() int {
-	counter += 1
-	return counter
+func getIncrement() func() int { // Step:1 - Outer Function
+	var counter int           //Step:2 - Variable in the outer function
+	increment := func() int { //Step:3 - Inner Function
+		counter += 1 //Step:4 - Variable in the outer function is used in the inner function
+		return counter
+	}
+	return increment //Step:5 - extend the lifetime of the inner function beyond the lifetime of the outer function (in this case - returing the inner function)
 }
