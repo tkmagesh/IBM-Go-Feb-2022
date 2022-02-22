6f57969893c0d7e078078e55c489195f44aa5b4c
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-22 14:33:31 +0530

    Slices - part-1

--- 04-collections/program.go
+++ 04-collections/program.go
@@ -25,4 +25,34 @@ func main() {
 	for _, val := range nos {
 		fmt.Println(val)
 	}
+
+	//Slice
+	fmt.Printf("\nSlice\n")
+	//var products []string
+	//var products []string = []string{"Pen", "Pencil", "Marker"}
+	//var products []string = []string{}
+
+	var products = make([]string, 0, 20)
+	products = append(products, "Pen", "Pencil", "Marker")
+
+	/*
+		var products = make([]string, 3, 20)
+		products[0] = "Pen"
+		products[1] = "Pencil"
+		products[2] = "Marker"
+	*/
+	fmt.Printf("%#v, len=%d, capacity=%d\n", products, len(products), cap(products))
+	//using the append function
+	/*
+		fmt.Println("After appending 1 new product")
+		products = append(products, "Scribble-Pad")
+		fmt.Printf("%#v, len=%d, capacity=%d\n", products, len(products), cap(products))
+	*/
+	fmt.Println("After appending 4 new products")
+	products = append(products, "Stapler", "Mouse", "Adapter", "Charger")
+	fmt.Printf("%#v, len=%d, capacity=%d\n", products, len(products), cap(products))
+
+	fmt.Println("After appending 1 more new product")
+	products = append(products, "IPad")
+	fmt.Printf("%#v, len=%d, capacity=%d\n", products, len(products), cap(products))
 }
