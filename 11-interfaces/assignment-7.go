fa405f1bacd284c4678f1ae39e30d4926cd37f3a
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-23 16:50:18 +0530

    assignment:7 - solution

--- 11-interfaces/assignment-7.go
+++ 11-interfaces/assignment-7.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type Product struct {
 	Id       int
@@ -26,6 +29,72 @@ func (products Products) String() string {
 	return result
 }
 
+func (products Products) Sort(attrName string) {
+	switch attrName {
+	case "Id":
+		sort.Sort(products)
+	case "Name":
+		sort.Sort(byName{products})
+	case "Cost":
+		sort.Sort(byCost{products})
+	case "Units":
+		sort.Sort(byUnits{products})
+	case "Category":
+		sort.Sort(byCategory{products})
+	default:
+		sort.Sort(products)
+	}
+
+}
+
+func (products Products) Len() int {
+	return len(products)
+}
+
+func (products Products) Swap(i, j int) {
+	products[i], products[j] = products[j], products[i]
+}
+
+func (products Products) Less(i, j int) bool {
+	return products[i].Id < products[j].Id
+}
+
+//sort by Name
+type byName struct {
+	Products
+}
+
+func (products byName) Less(i, j int) bool {
+	return products.Products[i].Name < products.Products[j].Name
+}
+
+//sort by Cost
+type byCost struct {
+	Products
+}
+
+func (products byCost) Less(i, j int) bool {
+	return products.Products[i].Cost < products.Products[j].Cost
+}
+
+//sort by Units
+type byUnits struct {
+	Products
+}
+
+func (products byUnits) Less(i, j int) bool {
+	return products.Products[i].Units < products.Products[j].Units
+}
+
+//sort by Name
+type byCategory struct {
+	Products
+}
+
+func (products byCategory) Less(i, j int) bool {
+	return products.Products[i].Category < products.Products[j].Category
+}
+
 func main() {
 	products := Products{
 		Product{105, "Pen", 5, 50, "Stationary"},
@@ -39,6 +108,30 @@ func main() {
 	//fmt.Println(products.Format())
 	fmt.Println(products)
 
+	fmt.Println("Default Sort")
+	//sort.Sort(products)
+	products.Sort("Id")
+	fmt.Println(products)
+
+	fmt.Println("Sort by name")
+	//sort.Sort(byName{products})
+	products.Sort("Name")
+	fmt.Println(products)
+
+	fmt.Println("Sort by cost")
+	//sort.Sort(byCost{products})
+	products.Sort("Cost")
+	fmt.Println(products)
+
+	fmt.Println("Sort by units")
+	//sort.Sort(byUnits{products})
+	products.Sort("Units")
+	fmt.Println(products)
+
+	fmt.Println("Sort by category")
+	//sort.Sort(byCategory{products})
+	products.Sort("Category")
+	fmt.Println(products)
 }
 
 /*
