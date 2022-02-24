6f38bcfe70e36400d4e5f01156ce7410de7c717a
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-24 12:21:54 +0530

    using close() and range construct with channels

---
+++ 12-concurrency/demo-11.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	evenCh := genEvenNos()
+	for {
+		if no, ok := <-evenCh; ok {
+			fmt.Println(no)
+		} else {
+			break
+		}
+	}
+	fmt.Println("main completed")
+}
+
+func genEvenNos() <-chan int {
+	var evenNoCh = make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			time.Sleep(500 * time.Millisecond)
+			evenNoCh <- i * 2
+		}
+		fmt.Println("All even nos are generated")
+		close(evenNoCh)
+	}()
+	return evenNoCh
+}
