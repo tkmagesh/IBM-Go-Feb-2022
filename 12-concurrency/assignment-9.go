9e20b8318763f8ec3b9629670b63167d97d38256
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-24 12:08:19 +0530

    assignment-9 : solution (partial)

--- 12-concurrency/assignment-9.go
+++ 12-concurrency/assignment-9.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	wg := &sync.WaitGroup{}
 	fmt.Println("main started")
-	print("hello")
-	print("world")
+	wg.Add(2)
+	go print("hello", ch1, ch2, wg)
+	go print("world", ch2, ch1, wg)
+	ch1 <- "start"
+	wg.Wait()
 	fmt.Println("main completed")
 }
 
-func print(str string) {
+func print(str string, in chan string, out chan string, wg *sync.WaitGroup) {
 	for i := 0; i < 5; i++ {
+		<-in
 		time.Sleep(500 * time.Millisecond)
 		fmt.Println(str)
+		out <- "done"
 	}
+	wg.Done()
 }
 
 /*
